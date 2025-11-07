6a2bdf738e2c3c0102a89dafb0c7377a7f377f8e
Samecho <zhayutong@gmail.com>
2025-11-06 23:33:12 -0800

    initial commit

---
+++ src/main/java/com/expense_reporting_system/expense_reporting_system/ExpenseReportingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.expense_reporting_system.expense_reporting_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReportingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReportingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expense_reporting_system/expense_reporting_system/ExpenseReportingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expense_reporting_system.expense_reporting_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseReportingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
